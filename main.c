e386dba6881c05a074034ed86b6343c56ad5fd26
Kant1deux3 <91676498+Kant1deux3@users.noreply.github.com>
2021-10-01 09:50:07 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	//Déclaration des variables
+	int nb;
+	int tabr[8];
+	int i;
+	char rec;
+
+	//Boucle de répétition du programme
+	do
+	{
+
+		//Invitation et lecture
+		do
+		{
+			printf("Donnez un nombre entre 0 et 255.\n");
+			scanf("%d", &nb);
+		}
+		while(nb<0||nb>255);
+
+
+		//initialiser la valeur du tableau à 0
+		i=0;
+		while (i!=8)
+		{
+			tabr[i]=0;
+			i++;
+		}
+
+		i=0;
+
+		//Effectuer les calculs et afficher les divisions successives
+    printf(" %d|2\n", nb );
+		do
+		{
+			tabr[i]=nb%2;
+			nb=nb/2;
+      printf("    %d|%d|2\n", tabr[i], nb);
+			i=i+1;
+
+		}while(nb!=0);
+
+
+		//Afficher le résultat
+		printf("Le résultat binaire est %d%d%d%d%d%d%d%d",tabr[7], tabr[6], tabr[5], tabr[4], tabr[3], tabr[2], tabr[1], tabr[0]);
+
+		printf("\nVoulez-vous recommencer ?\n");
+		scanf("%s", &rec);
+	}while(rec !='n');
+  printf("\nFin");
+	return 0;
+
+}
